4567b0a3c1c595f706e72c09f898d10a2efc1f39
kaua_amelo <kaua.amelo2020@gmail.com>
2025-02-17 01:23:01 -0300

    feat: resolvendo a sexta missao

---
+++ sextaMissao.py
@@ -0,0 +1,11 @@
+# Missão 6: Reforçando a Segurança e a Contagem do Sistema 💾
+# O vírus está comprometendo o sistema de segurança e a contagem de registros!
+# Para restaurar o funcionamento correto, você precisa reforçar as verificações
+# e garantir que os dados sejam processados corretamente.
+  # Exiba os números de 1 a 10 usando um loop while.
+
+contador = 1
+
+while contador <= 10:
+  print(contador)
+  contador += 1
